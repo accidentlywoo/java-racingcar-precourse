75bf9789541b2d4e0f02bd3b4e8714590b1af55d
accidentlywoo <accidentlywoo@gmail.com>
2021-04-30 23:22:28 +0900

    feat(게임 실행 RacingCarGame) : 게임 실행 클래스 생성

---
+++ src/main/java/RacingCarGame.java
@@ -0,0 +1,9 @@
+import ui.ConsoleInputOutController;
+
+public class RacingCarGame {
+	private static final ConsoleInputOutController consoleInputOutController = new ConsoleInputOutController();
+
+	public static void playGame(){
+		consoleInputOutController.run();
+	}
+}
