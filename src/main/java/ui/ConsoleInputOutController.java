39f05db0aabd7c164d28dc79cce60287db88958d
accidentlywoo <accidentlywoo@gmail.com>
2021-05-02 15:11:26 +0900

    feat(UI 콘솔 입출력 컨트롤러) : 입 출력 흐름 쪼개기
    자동차 이름받는 flow와 레이싱 횟수 받는 flow 먼저 나눔

--- src/main/java/ui/ConsoleInputOutController.java
+++ src/main/java/ui/ConsoleInputOutController.java
@@ -9,14 +9,40 @@ public class ConsoleInputOutController {
 	private final Scanner scanner = new Scanner(System.in);
 
 	public void run() {
-		System.out.println(ConsoleInputOutMessage.ASK_RACING_CAR_NAME);
-		new CarGroupGenerator(scanner.nextLine().trim());
+		boolean flag = true;
+
+		while (flag){
+			System.out.println(ConsoleInputOutMessage.ASK_RACING_CAR_NAME);
+
+			boolean carNameCheck = carNameCheck(scanner.nextLine().trim());
+
+			flag = racingCount(scanner.nextLine().trim(), carNameCheck);
+		}
+	}
+
+	private boolean carNameCheck(String inputCarNames){
+		try{
+			new CarGroupGenerator(inputCarNames);
+		}catch (IllegalArgumentException e){
+			System.out.println(e);
+			return false;
+		}
+		return true;
+	}
+
+	private boolean racingCount(String inputCarNames, boolean flag){
+		if(!flag){
+			return false;
+		}
 
 		System.out.println(ConsoleInputOutMessage.ASK_RACING_COUNT);
-		new RaceGenerator(scanner.nextLine().trim());
+
+		new RaceGenerator(inputCarNames);
 
 		System.out.println(ConsoleInputOutMessage.RACING_RESULT_MESSAGE);
 		// TODO 레이싱 뷰 뿌리기
 		System.out.println(ConsoleInputOutMessage.WINNER_SURFIX_MESSAGE);
+
+		return true;
 	}
 }
