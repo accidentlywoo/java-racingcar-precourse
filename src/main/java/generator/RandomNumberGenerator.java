8929705d401d9244f0c085fc00cdf3ec9c125ddf
accidentlywoo <accidentlywoo@gmail.com>
2021-05-03 23:56:25 +0900

    feat(Domain 전진 여부생성) : 레이싱 그룹 전진 여부 생성 설계

---
+++ src/main/java/generator/RandomNumberGenerator.java
@@ -0,0 +1,28 @@
+package generator;
+
+import domain.Car;
+import domain.CarGroup;
+
+import java.util.Random;
+
+import static util.RacingNumberConstant.*;
+
+public class RandomNumberGenerator {
+  CarGroup carGroup;
+
+  public RandomNumberGenerator(CarGroup raceCarGroup) {
+    this.carGroup = raceCarGroup;
+  }
+
+  public void gogoCar(){
+    final Car[] carList = carGroup.getCarList();
+    for(Car car : carList){
+
+      Random random = new Random();
+
+      int randomNumber = random.nextInt(MAX_RANDOM_NUMBER) + 1;
+
+      car.plusRunCount(randomNumber > EDGE_NUMBER_OF_GO ? GO : STOP);
+    }
+  }
+}
