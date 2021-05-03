bc7675c3424eb408fbf971d57ea806020f43b869
accidentlywoo <accidentlywoo@gmail.com>
2021-05-03 23:57:17 +0900

    feat : 레이싱 랜덤 숫자범위, 전진 여부 범위, 전진 / 멈춤 계수 상수표현

---
+++ src/main/java/util/RacingNumberConstant.java
@@ -0,0 +1,8 @@
+package util;
+
+public class RacingNumberConstant {
+	public static final Integer MAX_RANDOM_NUMBER = 9;
+	public static final Integer EDGE_NUMBER_OF_GO = 3;
+	public static final Integer GO = 1;
+	public static final Integer STOP = 0;
+}
