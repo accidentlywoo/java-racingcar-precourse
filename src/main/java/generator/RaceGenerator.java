fdea5699563b96f9a59583f48ea816387ce296b8
accidentlywoo <accidentlywoo@gmail.com>
2021-05-04 00:12:23 +0900

    feat(Generator 자동차 그룹 레이싱) : 레이싱 생성자 자동차 그룹 달리는 메소드 설계

--- src/main/java/generator/RaceGenerator.java
+++ src/main/java/generator/RaceGenerator.java
@@ -4,8 +4,19 @@ import domain.CarGroup;
 
 public class RaceGenerator {
   private final Integer count;
+  private final CarGroup carGroup;
 
-  public RaceGenerator(Integer count, CarGroup raceCarGroup) {
+  public RaceGenerator(Integer count, CarGroup carGroup) {
     this.count = count;
+    this.carGroup = carGroup;
+  }
+
+  public void run() {
+    final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(carGroup);
+
+    for (int i = 0; i < count; i ++){
+      randomNumberGenerator.gogoCar();
+    }
+
   }
 }
