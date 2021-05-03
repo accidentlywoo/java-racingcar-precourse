4c63adb32cec0033d58e4b1506b44ae1d7ca4f15
accidentlywoo <accidentlywoo@gmail.com>
2021-05-04 01:43:28 +0900

    test : 랜덤 수 생성 단위 테스트 케이스
    더 정교하게 테스트할 수 없나?
    클래스를 쪼개니 픽스처 만드는 과정이 조잡하다.
    뭔가 잘못된것같다

---
+++ src/test/java/generator/RandomNumberGeneratorTest.java
@@ -0,0 +1,42 @@
+package generator;
+
+import domain.CarGroup;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomNumberGeneratorTest {
+	@Test
+	public void 랜덤수_생성_후_3_이상_GO_미만_STOP(){
+	    // given
+		String test1 = "test1";
+		String test2 = "test2";
+		String test3 = "test3";
+
+		String[] cars = new String[]{test1, test2, test3};
+
+		CarGroupGenerator carGroupGenerator = new CarGroupGenerator(cars);
+
+		final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(carGroupGenerator.getRaceCarGroup());
+
+		// when
+		final CarGroup carGroup = randomNumberGenerator.gogoCar();
+
+		// than
+		assertThat(carGroup.getCars()).isNotEmpty();
+
+		assertThat(carGroup.getCars())
+				.extracting("name")
+				.contains(
+						test1,
+						test2,
+						test3
+				);
+
+		assertThat(carGroup.getCars())
+				.extracting("runCount")
+				.isNotEqualTo(0);
+	}
+}
