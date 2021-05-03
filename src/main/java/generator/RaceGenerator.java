c9b24acfaedfe95643eb579a585cfcf8d563d28f
accidentlywoo <accidentlywoo@gmail.com>
2021-05-04 01:26:23 +0900

    feat(Generator 자동차 그룹 레이싱) : 자동차 경주 승리자 리스트 뽑기.
    역할을 분리할 수 있을 것같다 -> 클래스 쪼개기

--- src/main/java/generator/RaceGenerator.java
+++ src/main/java/generator/RaceGenerator.java
@@ -1,6 +1,11 @@
 package generator;
 
+import domain.Car;
 import domain.CarGroup;
+import ui.RacingDrawing;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RaceGenerator {
   private final Integer count;
@@ -13,10 +18,28 @@ public class RaceGenerator {
 
   public void run() {
     final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(carGroup);
+    CarGroup cars;
 
     for (int i = 0; i < count; i ++){
-      randomNumberGenerator.gogoCar();
-    }
+      cars = randomNumberGenerator.gogoCar();
 
+      new RacingDrawing(cars.getCars());
+    }
+  }
+
+  public List<String> getWinner(){
+    List<String> result = new ArrayList<>();
+
+    int max = 0;
+
+    for(Car car : carGroup.getCars()){
+      max = car.getRunCount() > max ?
+              car.getRunCount() : max;
+
+      if (car.getRunCount() == max) {
+        result.add(car.getName());
+      }
+    }
+    return result;
   }
 }
