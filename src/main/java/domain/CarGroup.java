351ffdfb61bd569a21ce0b121b83ac89b87707cb
accidentlywoo <accidentlywoo@gmail.com>
2021-05-03 22:44:20 +0900

    feat(Domain 자동차 그룹) : 자동차 그룹 도메인 생성
    일급객체 사용

--- src/main/java/domain/CarGroup.java
+++ src/main/java/domain/CarGroup.java
@@ -3,11 +3,19 @@ package domain;
 public class CarGroup {
   private Car[] cars;
 
-  public CarGroup(Integer carsLength) {
-    cars = new Car[carsLength];
+  public CarGroup(String[] cars) {
+    this.cars = new Car[cars.length];
+
+    for (int i = 0; i < cars.length; i++){
+      putCarInGroup(new Car(cars[i]), i);
+    }
   }
 
-  public void putCarInGroup(Car car, Integer index){
+  private void putCarInGroup(Car car, Integer index){
     cars[index] = car;
+  }
+
+  public Car[] getCarGroup(){
+    return this.cars;
   }
 }
