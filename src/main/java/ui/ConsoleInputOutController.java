08666831580dbf5941cd8d9e052c833e71ccd844
accidentlywoo <accidentlywoo@gmail.com>
2021-05-03 22:49:45 +0900

    style : tab -> 2space

--- src/main/java/ui/ConsoleInputOutController.java
+++ src/main/java/ui/ConsoleInputOutController.java
@@ -6,62 +6,62 @@ import generator.RaceGenerator;
 import java.util.Scanner;
 
 public class ConsoleInputOutController {
-	private final Scanner scanner = new Scanner(System.in);
+  private final Scanner scanner = new Scanner(System.in);
+  private CarGroupGenerator carGroupGenerator;
 
-	public void run() {
-		boolean flag = true;
+  public void run() {
+    boolean flag = true;
 
-		while (flag){
-			System.out.println(ConsoleInputOutMessage.ASK_RACING_CAR_NAME);
+    while (flag){
+      System.out.println(ConsoleInputOutMessage.ASK_RACING_CAR_NAME);
 
-			boolean carNameCheck = carNameCheck(scanner.nextLine().trim());
+      boolean carNameCheck = carNameCheck(scanner.nextLine().trim());
 
-			System.out.println(ConsoleInputOutMessage.ASK_RACING_COUNT);
+      System.out.println(ConsoleInputOutMessage.ASK_RACING_COUNT);
 
-			flag = racingCount(scanner.nextLine().trim(), carNameCheck);
-		}
-	}
+      flag = racingCount(scanner.nextLine().trim(), carNameCheck);
+    }
+  }
 
-	private boolean carNameCheck(String inputCarNames){
-		try{
-			new CarGroupGenerator(inputCarNames);
+  private boolean carNameCheck(String inputCarNames){
+    try{
+      carGroupGenerator = new CarGroupGenerator(inputCarNames);
 
-		}catch (IllegalArgumentException e){
+    }catch (IllegalArgumentException e){
 
-			System.out.println(e);
+      System.out.println(e);
 
-			return false;
-		}
-		return true;
-	}
+      return false;
+    }
+    return true;
+  }
 
-	private boolean racingCount(String inputCarNames, boolean flag){
-		if(!flag){
-			return true;
-		}
+  private boolean racingCount(String inputCarNames, boolean flag){
+    if(!flag){
+      return true;
+    }
 
-		countCheck(inputCarNames);
+    countCheck(inputCarNames);
 
-		return flag;
-	}
+    return flag;
+  }
 
-	private void countCheck(String inputCount){
-		try{
-			new RaceGenerator(inputCount);
+  private void countCheck(String inputCount){
+    try{
+      new RaceGenerator(inputCount, carGroupGenerator.getRaceCarGroup());
 
-		}catch (IllegalArgumentException e){
+    }catch (IllegalArgumentException e){
 
-			System.out.println(e);
-		}
+      System.out.println(e);
+    }
 
-		printView();
-	}
+    printView();
+  }
 
-	private void printView(){
-		System.out.println(ConsoleInputOutMessage.RACING_RESULT_MESSAGE);
+  private void printView(){
+    System.out.println(ConsoleInputOutMessage.RACING_RESULT_MESSAGE);
 
 		// TODO 레이싱 뷰 뿌리기
-		System.out.println(ConsoleInputOutMessage.WINNER_SURFIX_MESSAGE);
-
-	}
+    System.out.println(ConsoleInputOutMessage.WINNER_SURFIX_MESSAGE);
+  }
 }
