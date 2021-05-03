ead2862264a08e61b954247bed8fc9b17865f29a
accidentlywoo <accidentlywoo@gmail.com>
2021-05-04 01:22:01 +0900

    feat(UI 자동차 경주 출력) : 전진 상태 그리기

---
+++ src/main/java/ui/RacingDrawing.java
@@ -0,0 +1,24 @@
+package ui;
+
+import domain.Car;
+
+public class RacingDrawing {
+	private static final String RUN_ONE_STEP = "-";
+
+	public RacingDrawing(Car[] cars) {
+		System.out.println();
+		for(Car car : cars){
+			System.out.println(car.getName() + " : " + drawBar(car.getRunCount()));
+		}
+	}
+
+	private String drawBar(Integer count){
+		String draw = "";
+
+		for(int i = 0; i < count; i++ ){
+			draw += RUN_ONE_STEP;
+		}
+
+		return draw;
+	}
+}
