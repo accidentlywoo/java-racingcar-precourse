a69c95d5d7b25fb1b84d1cc342042e8a4f7b62ab
accidentlywoo <accidentlywoo@gmail.com>
2021-05-04 17:09:07 +0900

    test : 자동차 레이스 생성클래스 단위 테스트 리스트 업
    레이스 실행 메소드 실행 후 숭리자 리스트 메소드 실행 -> GREEN 테스트
    레이스 실행 메소드 실행 안하고, 승리자 리스트 메소드 실행 -> RED 테스트

--- src/test/java/generator/RaceGeneratorTest.java
+++ src/test/java/generator/RaceGeneratorTest.java
@@ -1,18 +1,52 @@
 package generator;
 
+import domain.CarGroup;
 import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RaceGeneratorTest {
 
   @Test
-  public void 자동차_레이싱_테스트(){
+  public void 자동차_레이싱_테스트_GREEN(){
     // given
+    final CarGroup carGroup = 자동차그룹_픽스처();
+
+    int InputCount = 10;
+
+    // when
+    RaceGenerator raceGenerator = new RaceGenerator(InputCount, carGroup);
+
+    raceGenerator.run();
+
+    raceGenerator.getWinRaceCar();
+    // than
+	}
+
+	@Test
+  @DisplayName("레이싱 생성기에서 run 메소드를 실행하지 않고, 승리자를 구하려 했을 때. ")
+	public void 자도차_레이싱_테스트_RED(){
+    // given
+    final CarGroup carGroup = 자동차그룹_픽스처();
+
+    int InputCount = 10;
+
+    // when // than
+    RaceGenerator raceGenerator = new RaceGenerator(InputCount, carGroup);
+
+    assertThatThrownBy(raceGenerator::getWinRaceCar)
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessageContaining("게임을 실행시켜야. 승리자를 구분할 수 있습니다.");
+
+	}
+
+	private CarGroup 자동차그룹_픽스처(){
     String test1 = "test1";
     String test2 = "test2";
     String test3 = "test3";
@@ -26,25 +60,12 @@ class RaceGeneratorTest {
     assertThat(carGroupGenerator.getRaceCarGroup()).isNotNull();
 
     assertThat(carGroupGenerator.getRaceCarGroup().getCars())
-      .extracting("name", "runCount")
-      .contains(
-          Tuple.tuple(test1, 0),
-          Tuple.tuple(test2, 0),
-          Tuple.tuple(test3, 0)
-      );
-
-    int InputCount = 10;
-    // when
-
-    RaceGenerator raceGenerator = new RaceGenerator(InputCount, carGroupGenerator.getRaceCarGroup());
-    final List<String> winnerList = raceGenerator.run();
-
-    // than
-    assertThat(winnerList)
-      .containsAnyOf(
-            test1,
-            test2,
-            test3
+        .extracting("name", "runCount")
+        .contains(
+            Tuple.tuple(test1, 0),
+            Tuple.tuple(test2, 0),
+            Tuple.tuple(test3, 0)
         );
-	}
+    return carGroupGenerator.getRaceCarGroup();
+  }
 }
